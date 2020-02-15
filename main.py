7b774784e59297252cb139d100d92e136d60f073
dying <inktechser@gmail.com>
2020-02-16 02:42:20 +0530

    ai

---
+++ main.py
@@ -0,0 +1,57 @@
+import keras
+from keras import backend as K
+from keras.layers.core import Dense, Activation
+from keras.optimizers import Adam
+from keras.metrics import categorical_crossentropy
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image
+from keras.models import Model
+from keras.applications import imagenet_utils
+from keras.layers import Dense,GlobalAveragePooling2D
+from keras.applications import MobileNet
+from keras.applications.mobilenet import preprocess_input
+import numpy as np
+from IPython.display import Image
+from keras.optimizers import Adam
+
+
+base_model=MobileNet(weights='imagenet',include_top=False) #imports the mobilenet model and discards the last 1000 neuron layer.
+
+x=base_model.output
+x=GlobalAveragePooling2D()(x)
+x=Dense(1024,activation='relu')(x) #we add dense layers so that the model can learn more complex functions and classify for better results.
+x=Dense(1024,activation='relu')(x) #dense layer 2
+x=Dense(512,activation='relu')(x) #dense layer 3
+preds=Dense(2,activation='softmax')(x) #final layer with softmax activation
+model=Model(inputs=base_model.input,outputs=preds)
+
+
+for i,layer in enumerate(model.layers):
+  print(i,layer.name)
+
+  for layer in model.layers:
+    layer.trainable=False
+# or if we want to set the first 20 layers of the network to be non-trainable
+for layer in model.layers[:20]:
+    layer.trainable=False
+for layer in model.layers[20:]:
+    layer.trainable=True
+
+train_datagen=ImageDataGenerator(preprocessing_function=preprocess_input) #included in our dependencies
+
+train_generator=train_datagen.flow_from_directory('selfword',
+                                                 target_size=(224,224),
+                                                 color_mode='rgb',
+                                                 batch_size=32,
+                                                 class_mode='categorical',
+                                                 shuffle=True)
+
+
+model.compile(optimizer='Adam',loss='categorical_crossentropy',metrics=['accuracy'])
+
+step_size_train=train_generator.n//train_generator.batch_size
+model.fit_generator(generator=train_generator,
+                   steps_per_epoch=step_size_train,
+                   epochs=10)
+
+model.save('mymodel.h5')
