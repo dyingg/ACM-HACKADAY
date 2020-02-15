a7ba2da1eedf5823a89a22589cd8837fd74ea5ea
dying <inktechser@gmail.com>
2020-02-16 02:45:27 +0530

    ok

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ x=GlobalAveragePooling2D()(x)
 x=Dense(1024,activation='relu')(x) #we add dense layers so that the model can learn more complex functions and classify for better results.
 x=Dense(1024,activation='relu')(x) #dense layer 2
 x=Dense(512,activation='relu')(x) #dense layer 3
-preds=Dense(2,activation='softmax')(x) #final layer with softmax activation
+preds=Dense(3,activation='softmax')(x) #final layer with softmax activation
 model=Model(inputs=base_model.input,outputs=preds)
 
 
